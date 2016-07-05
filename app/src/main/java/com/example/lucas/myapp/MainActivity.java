e08b25a7314fbbfd549800cb629f0d31e265cc41
Lucas <lucas.sirodrigues@gmail.com>
2016-07-05 20:05:08 -0300

    [Creating a tab to creating a contact and list tab]

--- app/src/main/java/com/example/lucas/myapp/MainActivity.java
+++ app/src/main/java/com/example/lucas/myapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TabHost;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +25,27 @@ public class MainActivity extends AppCompatActivity {
         emailTxt = (EditText) findViewById(R.id.txtEmail);
         addressTxt = (EditText) findViewById(R.id.txtAddress);
 
+        TabHost tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("creator");
+        tabSpec.setContent(R.id.tabCreator);
+        tabSpec.setIndicator("Creator");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("list");
+        tabSpec.setContent(R.id.tabContactList);
+        tabSpec.setIndicator("List");
+        tabHost.addTab(tabSpec);
+
+
         final Button addBtn = (Button) findViewById(R.id.btnAdd);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Your contact has been created", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         nameTxt.addTextChangedListener(new TextWatcher() {
             @Override
