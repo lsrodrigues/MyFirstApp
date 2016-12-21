1a7c74ea11e142dabff7ea9d266c88b4547cfe92
Lucas <lucas.sirodrigues@gmail.com>
2016-12-21 16:09:42 -0200

    SQLite CRUD

--- app/src/main/java/com/example/lucas/myapp/DatabaseHandler.java
+++ app/src/main/java/com/example/lucas/myapp/DatabaseHandler.java
@@ -1,8 +1,11 @@
 package com.example.lucas.myapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 
 /**
  * Created by lrodrigues on 21/12/2016.
@@ -21,18 +24,47 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             KEY_ADDRESS = "address",
             KEY_IMAGEURI = "imageUri";
 
-    public DatabaseHandler(Context context){
+    public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL("CREATE TABLE" + TABLE_CONTACTS + "{" + KEY_ID + "INTEGER PRIMARY KEY," + KEY_NAME + "TEXT," + KEY_PHONE + "TEXT," + KEY_EMAIL + "TEXT," + KEY_ADDRESS + "TEXT," + KEY_IMAGEURI + "TEXT}");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE" + TABLE_CONTACTS + "{" + KEY_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_NAME + "TEXT," + KEY_PHONE + "TEXT," + KEY_EMAIL + "TEXT," + KEY_ADDRESS + "TEXT," + KEY_IMAGEURI + "TEXT}");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
         onCreate(db);
     }
+
+    public void createContact(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PHONE, contact.getPhone());
+        values.put(KEY_EMAIL, contact.getEmail());
+        values.put(KEY_ADDRESS, contact.getAddress());
+        values.put(KEY_IMAGEURI, contact.getImageURI().toString());
+
+        db.insert(TABLE_CONTACTS, null, values);
+        db.close();
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_CONTACTS, new String[]{KEY_ID, KEY_NAME, KEY_PHONE, KEY_EMAIL, KEY_ADDRESS, KEY_IMAGEURI}, KEY_ID + "=?", new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), Uri.parse(cursor.getString(5)));
+        return contact;
+    }
+
 }
+
